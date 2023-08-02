4098473c912ecc932b1c514ce6cb4d490d5ca939
mirsaalf <mirsaalf@gmail.com>
2023-08-02 15:08:15 -0700

    Created Student class

---
+++ EFCore/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    public class Student { 
+    [Key] // Primary Key 
+    public int Id { get; set; }
+
+    public string FullName { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+    
+    }
+}
